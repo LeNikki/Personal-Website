9310ad47c1f9bec89968d562e179e74a375543a7
Nicole Satiembre <80201174+LeNikki@users.noreply.github.com>
2021-08-29 20:20:17 +0800

    Add files via upload

---
+++ Resume/js/script.js
@@ -0,0 +1,65 @@
+const parallax = document.getElementById("div1")
+
+window.addEventListener("scroll", function ()
+{
+    let offset = window.pageYOffset;
+    parallax.style.backgroundPositionY = offset*0.7 + "px";
+})
+
+//############ SLIDESHOW #############################
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides fade");
+  //var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+ /* for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }*/
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+  //Automatic SlideShow
+ /* var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides fade");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 10000); // Change image every 10 seconds
+}*/
+
+/* ################### MEDIA SCREEN #######################*/
+function myFunction(x) {
+    if (x.matches) { // If media query matches
+      document.body.style.color = "yellow";
+    } else {
+      document.body.style.backgroundColor = "pink";
+    }
+  }
+  
+  var x = window.matchMedia("(max-width: 600px)")
+  myFunction(x) // Call listener function at run time
+  x.addListener(myFunction) // Attach listener function on state changes
